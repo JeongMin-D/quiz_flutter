6a077cc28cf58f9d0581c73aaf344813e20a43c9
JeongMin-D <j25ng.py@gmail.com>
2021-02-09 02:29:50 +0900

    master

--- lib/screen/screen_tests.dart
+++ lib/screen/screen_tests.dart
@@ -26,56 +26,6 @@ class _QuiztestState extends State<Quiztest> {
   int _timer = timer;
   int life = 3;
 
-  void _showDialog_cor() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        Future.delayed(Duration(seconds: 1), () {
-          Navigator.pop(context);
-        });
-
-        return AlertDialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          content: SizedBox(
-            height: 200,
-            child: Center(
-                child: SizedBox(
-              child: Text("정답입니다."),
-              height: 50.0,
-              width: 50.0,
-            )),
-          ),
-        );
-      },
-    );
-  }
-
-  void _showDialog_wro() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        Future.delayed(Duration(seconds: 1), () {
-          Navigator.pop(context);
-        });
-
-        return AlertDialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-          content: SizedBox(
-            height: 200,
-            child: Center(
-                child: SizedBox(
-              child: Text("오답입니다."),
-              height: 50.0,
-              width: 50.0,
-            )),
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -291,12 +241,12 @@ class _QuiztestState extends State<Quiztest> {
                 }
               }
             });
-            if (widget.quizs[_currentIndex].answer == _answers[_currentIndex]) {
-              _showDialog_cor();
-            } else if (widget.quizs[_currentIndex].answer !=
-                _answers[_currentIndex]) {
-              _showDialog_wro();
-            }
+            // if (widget.quizs[_currentIndex].answer == _answers[_currentIndex]) {
+            //   _showDialog_cor();
+            // } else if (widget.quizs[_currentIndex].answer !=
+            //     _answers[_currentIndex]) {
+            //   _showDialog_wro();
+            // }
           },
         ),
       );
